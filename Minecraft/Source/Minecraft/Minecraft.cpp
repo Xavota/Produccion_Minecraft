18a576e58eac104715727dc380191f6614dcc196
rivaz64 <59717197+rivaz64@users.noreply.github.com>
2021-02-20 17:14:32 -0600

    a

--- Minecraft/Source/Minecraft/Minecraft.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Minecraft.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Minecraft, "Minecraft" );
- 
